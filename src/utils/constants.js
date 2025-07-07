e017b2a2e4dfd20da069707032ca3d5122a7df60
unknown <Shobhan>
2025-07-07 18:30:25 +0530

    Added frontend url to allowed origin

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,8 @@ module.exports = {
   SUCCESS_MESSAGE: "Email Sent Successfully",
   ERROR_MESSAGE: "FAILED TO SEND EMAIL",
   EMAIL_SUBJECT: "Your Quiz Score",
-  ALLOWED_ORIGINS: ["http://localhost:5173", "https://yourfrontendapp"],
+  ALLOWED_ORIGINS: [
+    "http://localhost:5173",
+    "https://shobhankumarrath.github.io/Test_my_knowledge/",
+  ],
 };
